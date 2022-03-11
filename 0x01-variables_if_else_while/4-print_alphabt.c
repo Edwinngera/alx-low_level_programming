8c26e9a1a7bbaf23d010e04dae296cdb867f389d
root <edwinngera@gmail.com>
2022-03-11 10:31:30 -0800

    not c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{       
+		if(c!='q'''&& c!='e')
+		{
+
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
