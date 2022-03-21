36f1d2759b4c2dd6880bfd1455b0f60ff98df061
root <edwinngera@gmail.com>
2022-03-21 11:16:50 -0700

    added puts task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * puts2 - prints one char out of 2
- * @str: type
+ * puts2 - print characte
+ * @str: input string
  *
+ * Description: prints character of a string, starting first character
+ * Return: Always (0)
  */
 
 void puts2(char *str)
 {
-	int i, j = 0;
+	int i;
 
 	while (str[i] != '\0')
 	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 		i++;
-	}
-
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
 	}
 	_putchar('\n');
 }
